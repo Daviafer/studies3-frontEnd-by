df924bef2dc6a54077a2a4d4b52da2021920781c
Daviafer <daviafer1@gmail.com>
2022-08-29 21:03:26 -0300

    Iniciando funções da calculadora

--- calculadora-react/src/components/Button.jsx
+++ calculadora-react/src/components/Button.jsx
@@ -1,3 +1,16 @@
 import React from "react"
-export default props => 
-<button className="button">0</button>
+
+export default props => {
+  let classes = 'button'
+  classes += props.operation ? 'operation' : ''
+  classes += props.double ? 'double' : ''
+  classes += props.triple ? 'triple' : ''
+
+  return (
+    <button
+      onClick={e => props.click && props.click(props.label)} 
+      className={classes}>
+      {props.label}
+    </button>
+  )
+} 
